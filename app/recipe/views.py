0d893752eae73a692eb4e6ade0165ed618cc1a54
Joshua Maxcy <kanemaxcy@gmail.com>
2019-06-18 23:54:47 -0400

    added mixin for adding new tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(GenericViewSet, ListModelMixin):
+class TagViewSet(GenericViewSet, ListModelMixin, CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -16,3 +16,7 @@ class TagViewSet(GenericViewSet, ListModelMixin):
     def get_queryset(self):
         """return objects for current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """create new tag"""
+        serializer.save(user=self.request.user)
--- app/tests/test_recipes.py
+++ app/tests/test_recipes.py
@@ -58,3 +58,22 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """test creating a new tag"""
+        payload = {'name': 'test'}
+        self.client.post(TAGS_URL, payload)
+
+        does_exist = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(does_exist)
+
+    def test_create_invalid_tag(self):
+        """test creating an invalid tag"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)

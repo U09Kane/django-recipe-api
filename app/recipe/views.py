0df67a97ba692cba92dd07ebfd8209d68c5e27aa
Joshua Maxcy <kanemaxcy@gmail.com>
2019-06-18 23:44:23 -0400

    added tag endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,18 @@
-# from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Tag
+from recipe import serializers
 
-# Create your views here.
+
+class TagViewSet(GenericViewSet, ListModelMixin):
+    """Manage tags in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """return objects for current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
